8c474e240f61eb1221ba904a3ff695daa3df45b0
Sabrina <sabrinasrimulyani@gmail.com>
2024-05-28 02:43:25 +0700

    refactor: added controller decorator

--- src/trips/use-cases/commands/update-trip/update-trip.controller.ts
+++ src/trips/use-cases/commands/update-trip/update-trip.controller.ts
@@ -1,8 +1,9 @@
-import { Inject, Put, Req } from '@nestjs/common';
+import { Controller, Inject, Put, Req } from '@nestjs/common';
 import { UpdateTripHandler } from './update-trip.handler';
 import { Request } from 'express';
 import { UPDATE_TRIP_HANDLER } from 'src/trips/trip.constants';
 
+@Controller()
 export class UpdateTripController {
   constructor(
     @Inject(UPDATE_TRIP_HANDLER) private handler: UpdateTripHandler,
